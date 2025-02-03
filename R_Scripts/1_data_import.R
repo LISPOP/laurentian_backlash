41bbb42a1aa02c9f5ee684e801bfa12014c2e558
Simon Kiss <sjkiss@gmail.com>
2025-02-03 09:44:59 -0500

    Uploaded folder for R Scripts

---
+++ R_Scripts/1_data_import.R
@@ -0,0 +1,9 @@
+#packages to install
+#Uncomment and execute 
+#install.packages("here")
+#install.packages("tidyverse")
+#Load here and readxl (readxl is part of the tidyverse package)
+library(here)
+library(readxl)
+library(tidyverse)
+
