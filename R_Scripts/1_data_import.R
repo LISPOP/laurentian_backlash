88d66edb391cd9c94d0aec29afb4aabd12fa14ba
Brynn Thomson <brynnthom.dg@gmail.com>
2025-02-10 10:03:49 -0500

    data import

--- R_Scripts/1_data_import.R
+++ R_Scripts/1_data_import.R
@@ -4,6 +4,10 @@
 #install.packages("tidyverse")
 #Load here and readxl (readxl is part of the tidyverse package)
 library(here)
-library(readxl)
 library(tidyverse)
-
+#push
+on18 <- read_excel("data/on2018_results.xlsx")
+on22<- read_excel("data/on2022_results.xlsx")
+glimpse(on18)
+on18
+view(on18)
