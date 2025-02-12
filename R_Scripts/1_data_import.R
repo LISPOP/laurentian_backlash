536f238572eed474330b72d918ff5c2ba93a2a3f
LeheckaN <Lehecka.n@gmail.com>
2025-02-12 13:24:48 -0500

    Update 1_data_import.R

--- R_Scripts/1_data_import.R
+++ R_Scripts/1_data_import.R
@@ -21,4 +21,4 @@ glimpse(on)
 library(dplyr)
 # Exclude specific elections by name
 on %>%
-  filter(EventNameEnglish != "Provincial By-elections" | IsGeneralElection != 0)
+  filter(IsGeneralElection != 0)
