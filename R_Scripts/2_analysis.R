e4be90ec1a45048c1db17cbf4fcd5046331b31a9
Brynn Thomson <brynnthom.dg@gmail.com>
2025-02-26 15:11:33 -0500

    Histogram for election years

--- R_Scripts/2_analysis.R
+++ R_Scripts/2_analysis.R
@@ -15,3 +15,16 @@ on_filtered %>%
   group_by(Election) %>%
   slice_min(mv, n=15) %>% view()
 
+library(ggplot2)
+
+print(colnames(on_filtered))
+
+colnames(on_filtered) <-tolower(colnames(on_filtered))
+
+ggplot(on_filtered, aes(x = mv)) +
+  geom_histogram(bins = 15, fill = "blue", color = "black") +  
+  facet_wrap(~ election) +
+  labs(title = "Histogram of Margin of Victory by Election Year",
+       x = "Margin of Victory",
+       y = "Frequency") +
+  theme_minimal()
