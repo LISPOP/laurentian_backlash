c98e951a92e761913e216dffbbde314a423c10f0
Simon Kiss <sjkiss@gmail.com>
2025-05-28 11:33:00 -0400

    May 28 some saved changes

--- R_Scripts/4_synthetic_control.R
+++ R_Scripts/4_synthetic_control.R
@@ -15,10 +15,12 @@ pc<-as.data.frame(pc)
 
 # neighbouring districts
 # Select unchanged northern boundaries
+pc %>% 
+  filter(northern==1&sudbury!="Sudbury") %>% 
+  filter(Year>2017) %>% 
+  select(District) %>% distinct() %>% unlist()->northern_controls
 
-northern_ridings
-
-data_francophone<-dataprep(foo=subset(pc, Year>2017), 
+data_northern<-dataprep(foo=subset(pc, Year>2017), 
          predictors=c("francophones_pct", "mining_pct", "income", "phds_pct",  "first_nations_pct", "density"),
          time.predictors.prior=2022,
          time.variable="Year",
@@ -26,31 +28,30 @@ data_francophone<-dataprep(foo=subset(pc, Year>2017),
          unit.variable="ED_ID",
          unit.names.variable = "District",
          treatment.identifier = "Sudbury",
-         controls.identifier = francophone,
+         controls.identifier = northern_controls,
          time.optimize.ssr = 2018,
          time.plot=c(2018,2022)
          )
 
-mod_francophone<-synth(data_francophone)
-summary(mod_francophone)
+mod_northern<-synth(data_northern)
+mod_northern
+#What are the weights of the controls?
+northern_tab<-synth.tab(mod_northern, data_northern)
+northern_tab
 #In the help documentation for path.plot
 # This is the equation that returns the trajectory for the synthetic unit
-data_francophone$Y0plot%*% mod_francophone$solution.w %>% 
+data_northern$Y0plot%*% mod_northern$solution.w %>% 
   #Convert to data frame
   as.data.frame() %>% 
   #add years
   mutate(Year=c(2018, 2022)) %>% 
   rename(Percent=w.weight) %>% 
-  mutate(Group=rep("Synthetic",2))->synthetic_results_francophone
-francophone$District
+  mutate(Group=rep("Synthetic",2))->synthetic_results_northern
 
-#Get control results
-francophone
-pc$District
+
+#Get control results=
 pc %>% 
-  subset(District %in% francophone)
-pc %>% 
-  filter(francophone==1) %>% 
+  filter(northern==1&sudbury!="Sudbury") %>% 
   filter(Year>2017) %>% 
   group_by(Year, District) %>% 
   select(Year, Percent) %>% group_by(Year) %>% 
@@ -61,7 +62,7 @@ pc %>%
   filter(., District=="Sudbury"&Year>2017) %>% 
   select(Year, Percent) %>% 
   mutate(Group=rep("Sudbury", 2))->sudbury_results
-synthetic_results %>% 
+synthetic_results_northern %>% 
   bind_rows(sudbury_results) %>% 
   bind_rows(., control_results) %>% 
   ggplot(., aes(x=as.factor(Year), 
@@ -69,61 +70,53 @@ synthetic_results %>%
                 fill=fct_relevel(Group, "Control", "Synthetic","Sudbury")))+
   geom_col(position="dodge")+labs(fill="Group", x="Election")+
   scale_fill_manual(values=c("darkblue", "lightblue", "cyan"))+theme(legend.position="bottom")
-data_neighbours<-dataprep(foo=subset(pc, Year>2017), 
-                           predictors=c("francophones_pct", "mining_pct","certificate_pct", "income", "phds_pct", "density", "first_nations_pct", "age"),
-                           time.predictors.prior=2018,
-                           time.variable="Year",
-                           dependent="Percent",
-                           unit.variable="ED_ID",
-                           unit.names.variable = "District",
-                           treatment.identifier = "Sudbury",
-                           controls.identifier = neighbours$District,
-                           time.optimize.ssr = 2018,
-                           time.plot=c(2018,2022)
-)
-
-
-mod_neighbours<-synth(data_neighbours)
-path.plot(mod_neighbours, data_neighbours)#Note Need a barplot
-#In the help documentation for path.plot
-# This is the equation that returns the trajectory for the synthetic unit
-data_neighbours$Y0plot%*% mod_neighbours$solution.w %>% 
-  #Convert to data frame
-  as.data.frame() %>% 
-  #add years
-mutate(Year=c(2018, 2022)) %>% 
-  rename(Percent=w.weight) %>% 
-  mutate(Group=rep("Synthetic",2))->synthetic_results
-
-#Get control results
-pc %>% 
-  filter(District%in% neighbours$District) %>% 
-  group_by(Year, District) %>% 
-  select(Year, Percent) %>% group_by(Year) %>% 
-  summarize(Percent=mean(Percent)) %>% 
-mutate(Group=rep("Control", nrow(.)))->control_results
-control_results
-pc %>% 
-  filter(., District=="Sudbury"&Year>2017) %>% 
-  select(Year, Percent) %>% 
-  mutate(Group=rep("Sudbury", 2))->sudbury_results
-synthetic_results %>% 
-  bind_rows(sudbury_results) %>% 
-  bind_rows(., control_results) %>% 
-  ggplot(., aes(x=as.factor(Year), 
-                y=Percent, 
-                fill=fct_relevel(Group, "Control", "Synthetic","Sudbury")))+
-  geom_col(position="dodge")+labs(fill="Group", x="Election")+
-  scale_fill_manual(values=c("darkblue", "lightblue", "cyan"))+theme(legend.position="bottom")
-ggsave(filename=here("Poster/synthetic_control.png"), width=12,height=8)
+# data_neighbours<-dataprep(foo=subset(pc, Year>2017), 
+#                            predictors=c("francophones_pct", "mining_pct","certificate_pct", "income", "phds_pct", "density", "first_nations_pct", "age"),
+#                            time.predictors.prior=2018,
+#                            time.variable="Year",
+#                            dependent="Percent",
+#                            unit.variable="ED_ID",
+#                            unit.names.variable = "District",
+#                            treatment.identifier = "Sudbury",
+#                            controls.identifier = neighbours$District,
+#                            time.optimize.ssr = 2018,
+#                            time.plot=c(2018,2022)
+# )
+# 
+# 
+# mod_neighbours<-synth(data_neighbours)
+# path.plot(mod_neighbours, data_neighbours)#Note Need a barplot
+# #In the help documentation for path.plot
+# # This is the equation that returns the trajectory for the synthetic unit
+# data_neighbours$Y0plot%*% mod_neighbours$solution.w %>% 
+#   #Convert to data frame
+#   as.data.frame() %>% 
+#   #add years
+# mutate(Year=c(2018, 2022)) %>% 
+#   rename(Percent=w.weight) %>% 
+#   mutate(Group=rep("Synthetic",2))->synthetic_results
+# 
+# #Get control results
+# pc %>% 
+#   filter(District%in% neighbours$District) %>% 
+#   group_by(Year, District) %>% 
+#   select(Year, Percent) %>% group_by(Year) %>% 
+#   summarize(Percent=mean(Percent)) %>% 
+# mutate(Group=rep("Control", nrow(.)))->control_results
+# control_results
+# pc %>% 
+#   filter(., District=="Sudbury"&Year>2017) %>% 
+#   select(Year, Percent) %>% 
+#   mutate(Group=rep("Sudbury", 2))->sudbury_results
+# synthetic_results %>% 
+#   bind_rows(sudbury_results) %>% 
+#   bind_rows(., control_results) %>% 
+#   ggplot(., aes(x=as.factor(Year), 
+#                 y=Percent, 
+#                 fill=fct_relevel(Group, "Control", "Synthetic","Sudbury")))+
+#   geom_col(position="dodge")+labs(fill="Group", x="Election")+
+#   scale_fill_manual(values=c("darkblue", "lightblue", "cyan"))+theme(legend.position="bottom")
+# ggsave(filename=here("Poster/synthetic_control.png"), width=12,height=8)
   #join back to vote results for sudbury
 
-#What are the weights of the controls?
-neighbours_tab<-synth.tab(mod_neighbours, data_neighbours)
-neighbours_tab
 
-# Try Northern Ontario Cities
-pc %>% 
-  filter(ElectoralDistrictName%in% neighbours$ElectoralDistrictName) %>% 
-  select(Year, ElectoralDistrictName, Percent)
-  
